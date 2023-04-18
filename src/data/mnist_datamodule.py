45fa36f7ee9019396653ed518dd43d62e6d9c50a
Cas Steigstra <cas.steigstra@gmail.com>
2023-04-18 18:10:24 +0200

    Dataset support for downloading and loading.

--- src/data/mnist_datamodule.py
+++ src/data/mnist_datamodule.py
@@ -1,13 +1,178 @@
-from typing import Any, Dict, Optional, Tuple
-
+from typing import Any, Dict, Optional, Tuple, Callable, List
+import os
+import warnings
 import torch
+from urllib.error import URLError
 from lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
-from torchvision.datasets import MNIST
+from torchvision.datasets.utils import _flip_byte_order, check_integrity, download_and_extract_archive, extract_archive, verify_str_arg
+import torch.utils.data as data
 from torchvision.transforms import transforms
+import numpy as np
 
+class MetaMaterial(data.Dataset):
+    mirrors = [
+        "https://zenodo.org/record/7070963/files",
+    ]
 
-class MNISTDataModule(LightningDataModule):
+    resources = [
+        ("Modescaling_classification_results.zip", "79e649816a7473310f00d69b4053dae8"),
+        ("Modescaling_raw_data.zip", "b498284b233f216f339b05c9c0438c8e")
+    ]
+
+    training_file = "training.pt"
+    test_file = "test.pt"
+    classes = [
+        "0 - zero",
+        "1 - one",
+    ]
+
+    @property
+    def train_labels(self):
+        warnings.warn("train_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def test_labels(self):
+        warnings.warn("test_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def train_data(self):
+        warnings.warn("train_data has been renamed data")
+        return self.data
+
+    @property
+    def test_data(self):
+        warnings.warn("test_data has been renamed data")
+        return self.data
+
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        download: bool = False,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform)
+        self.train = train  # training set or test set
+
+        if self._check_legacy_exist():
+            self.data, self.targets = self._load_legacy_data()
+            return
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError("Dataset not found. You can use download=True to download it")
+
+        self.data, self.targets = self._load_data()
+
+    def _check_legacy_exist(self):
+        processed_folder_exists = os.path.exists(self.processed_folder)
+        if not processed_folder_exists:
+            return False
+
+        return all(
+            check_integrity(os.path.join(self.processed_folder, file)) for file in (self.training_file, self.test_file)
+        )
+
+    def _load_legacy_data(self):
+        # This is for BC only. We no longer cache the data in a custom binary, but simply read from the raw data
+        # directly.
+        data_file = self.training_file if self.train else self.test_file
+        return torch.load(os.path.join(self.processed_folder, data_file))
+
+    def _load_data(self):
+        # TODO: Add support for sizes as arguments.
+        grid_file = f"data_new_rrQR_i_n_M_3x3_fixn4.npy"
+        data = torch.from_numpy(np.load(os.path.join(self.raw_folder, grid_file)).astype(int))
+
+        # Load classification results, class I or C.
+        label_file = f"results_analysis_new_rrQR_i_Scen_slope_offset_M1k_3x3_fixn4"
+        targets = torch.from_numpy(np.loadtxt(os.path.join(self.raw_folder, f"{label_file}.txt"), delimiter=',').astype(int))
+        # Combine with extended experiments.
+        label_ext_file = f"{label_file}_classX_extend"
+        targets_ext = torch.from_numpy(np.loadtxt(os.path.join(self.raw_folder, f"{label_ext_file}.txt"), delimiter=',').astype(int))
+        targets[targets_ext[:, 0]] = targets_ext
+
+        return data, targets
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        grid, target = self.data[index], int(self.targets[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        # TODO: SUPPORT FOR GRIDS :)
+        img = Image.fromarray(img.numpy(), mode="L")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @property
+    def raw_folder(self) -> str:
+        return os.path.join(self.root, self.__class__.__name__, "raw")
+
+    @property
+    def processed_folder(self) -> str:
+        return os.path.join(self.root, self.__class__.__name__, "processed")
+
+    @property
+    def class_to_idx(self) -> Dict[str, int]:
+        return {_class: i for i, _class in enumerate(self.classes)}
+
+    def _check_exists(self) -> bool:
+        return all(
+            check_integrity(os.path.join(self.raw_folder, os.path.splitext(os.path.basename(url))[0]))
+            for url, _ in self.resources
+        )
+
+    def download(self) -> None:
+        """Download the MNIST data if it doesn't exist already."""
+
+        if self._check_exists():
+            return
+
+        os.makedirs(self.raw_folder, exist_ok=True)
+
+        # download files
+        for filename, md5 in self.resources:
+            for mirror in self.mirrors:
+                url = f"{mirror}{filename}"
+                try:
+                    print(f"Downloading {url}")
+                    download_and_extract_archive(url, download_root=self.raw_folder, filename=filename, md5=md5)
+                except URLError as error:
+                    print(f"Failed to download (trying next):\n{error}")
+                    continue
+                finally:
+                    print()
+                break
+            else:
+                raise RuntimeError(f"Error downloading {filename}")
+
+    def extra_repr(self) -> str:
+        split = "Train" if self.train is True else "Test"
+        return f"Split: {split}"
+
+class MetaMaterialDataModule(LightningDataModule):
     """Example of LightningDataModule for MNIST dataset.
 
     A DataModule implements 6 key methods:
@@ -41,6 +206,7 @@ class MNISTDataModule(LightningDataModule):
         batch_size: int = 64,
         num_workers: int = 0,
         pin_memory: bool = False,
+        transform: Optional[transforms.Compose] = None,
     ):
         super().__init__()
 
@@ -49,9 +215,7 @@ class MNISTDataModule(LightningDataModule):
         self.save_hyperparameters(logger=False)
 
         # data transformations
-        self.transforms = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        )
+        self.transforms = transform
 
         self.data_train: Optional[Dataset] = None
         self.data_val: Optional[Dataset] = None
